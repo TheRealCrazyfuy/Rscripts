e8bd0863ddf7842efdc4f82b99dd8a41fe3ac7ec
TheRealCrazyfuy <108433136+TheRealCrazyfuy@users.noreply.github.com>
2022-06-30 14:55:48 +0200

    Add files via upload

---
+++ slidehousetumble.lua
@@ -0,0 +1,15 @@
+
+if game.PlaceId == 8438158667 then
+_G.enabled = true
+    while _G.enabled == true do
+        game.StarterGui:SetCore("SendNotification", {Title = "Farm wins Slide House Tumble", Text = "Made by GHOST software https://discord.gg/8MnUGh8p4S", Duration = 60})
+        game.StarterGui:SetCore("SendNotification", {Title = "Use in Autoexec!!!", Text = "", Duration = 60})
+        repeat wait() until game:IsLoaded()
+            wait(1)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-207.63079833984375, 4864.16015625, -57772.69140625)
+            wait(1)
+            local module = loadstring(game:HttpGet('https://ghost-storage.7m.pl/scripts/serverhop.lua'))()
+            module:Teleport(game.PlaceId)
+            wait()
+    end
+end
