9d0ec3b4f528114eace0f3aa0b66c2ea4987accb
TheRealCrazyfuy <108433136+TheRealCrazyfuy@users.noreply.github.com>
2023-03-23 19:25:11 +0100

    Remove an extra tab

--- EasyLadderClimbObby.lua
+++ EasyLadderClimbObby.lua
@@ -5,7 +5,7 @@ while _G.loop == true do
 		if v.Name == "TouchInterest" then
 		firetouchinterest(game.Players.LocalPlayer.Character.Head, v.Parent, 0)
 		wait(0.1)
-			firetouchinterest(game.Players.LocalPlayer.Character.Head, v.Parent, 1)
+		firetouchinterest(game.Players.LocalPlayer.Character.Head, v.Parent, 1)
 		end
 	end
 end
