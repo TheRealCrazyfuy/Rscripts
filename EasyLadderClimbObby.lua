da37f9a7b342dd4c94ea2c7fd7f90d1ba5573424
TheRealCrazyfuy <108433136+TheRealCrazyfuy@users.noreply.github.com>
2023-03-23 19:09:01 +0100

    Initial release

---
+++ EasyLadderClimbObby.lua
@@ -0,0 +1,11 @@
+--https://www.roblox.com/games/12129695366/Easy-Ladder-Climb-Obby
+_G.loop = true --change to false to stop
+while _G.loop == true do
+	for i,v in pairs(game:GetService("Workspace").Map.Folder:GetDescendants()) do
+		if v.Name == "TouchInterest" then
+		firetouchinterest(game.Players.LocalPlayer.Character.Head, v.Parent, 0)
+		wait(0.1)
+			firetouchinterest(game.Players.LocalPlayer.Character.Head, v.Parent, 1)
+		end
+	end
+end
